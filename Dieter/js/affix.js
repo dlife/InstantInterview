7c29b80c58cc7a38bf1044824f73904d5de2346a
dlife <dieterbenoot@gmail.com>
2015-05-12 16:15:15 +0200

    Affix Scrollspy en scrollAnimate working

--- Dieter/js/affix.js
+++ Dieter/js/affix.js
@@ -6,3 +6,15 @@ $('#nav').affix({
         top: $('header').height()
     }
 });
+
+$('section').css("height", $(window).height());
+
+$('#scroll').scrollspy({target: '#nav'});
+
+$('.scrollAnimate').click(function(event){
+    // preventDefault zorgt voor een smooth overgang
+    event.preventDefault();
+    $('html, body').animate({
+            scrollTop: $(this.children[0].attributes[0].value).offset().top}, 1000
+    );
+});
