90aa682bd8bdd968ee730810303f09c9ffed5452
cedricjacobs <cedric.jacobs@icloud.com>
2015-05-16 20:13:14 +0200

    Update SQL

--- Cedric/TestStoredProcedure.php
+++ Cedric/TestStoredProcedure.php
@@ -86,13 +86,21 @@ try {
     echo($preparedStatement->rowCount());*/
 
     // TEST Update specific functie
-    $preparedStatement = $cn->prepare("call FunctieUpdate(:pId, :pName);");
+/*    $preparedStatement = $cn->prepare("call FunctieUpdate(:pId, :pName);");
     $FId = 1;
     $naam = "UpdateTestFunctie";
     $preparedStatement->bindParam(':pId',$FId,PDO::PARAM_INT);
     $preparedStatement->bindParam(':pName',$naam,PDO::PARAM_INT);
     $result = $preparedStatement->execute();
-    echo($preparedStatement->rowCount());
+    echo($preparedStatement->rowCount());*/
+
+    // TEST Update specific functie
+        $preparedStatement = $cn->prepare("call FunctieUpdate(:pId, :pName);");
+        $FId = 1;
+        $preparedStatement->bindParam(':pId',$FId,PDO::PARAM_INT);
+        $result = $preparedStatement->execute();
+        echo($preparedStatement->rowCount());
+
 }
 catch (\PDOException $e)
 {
