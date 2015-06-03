1e38adeb46ae2a20136ccd6b1cb8125a4e51d843
cedricjacobs <cedric.jacobs@icloud.com>
2015-06-03 22:14:03 +0200

    we kunnen pdf's maken

--- Root/BLL/CreatePdf.php
+++ Root/BLL/CreatePdf.php
@@ -11,6 +11,7 @@ $fId = 2;
 $pdf->ParseData($array,$fId);
 $pdf->GetData();
 $pdf->BuildPdf();
+//$pdf->ExportPDF();
 
 class createPdf
 {
@@ -19,39 +20,38 @@ class createPdf
     protected $questionString;
     protected $PdfData;
     protected $Pdf;
+    protected $name;
+
     public function ParseData($array, $funcId)
     {
         try {
             $funcId = intval($funcId);
-            print $funcId.'<Br>';
-            foreach($array as $val)
-            {
-                $valint=intval($val);
-                if($valint!=0){
-                    if(strlen($this->questionString)!=0) {
+           // print $funcId . '<Br>';
+            foreach ($array as $val) {
+                $valint = intval($val);
+                if ($valint != 0) {
+                    if (strlen($this->questionString) != 0) {
                         $this->questionString .= ",$valint";
-                        print $this->questionString . '<Br>';
-                    }
-                    else{
+            //            print $this->questionString . '<Br>';
+                    } else {
                         $this->questionString .= "$valint";
-                        print $this->questionString . '<Br>';
+               //         print $this->questionString . '<Br>';
                     }
                 }
             }
-        }
-        catch(Exception $e)
-        {
-           echo $e;
+        } catch (Exception $e) {
+            echo $e;
         }
 
     }
 
-    public function GetData ()
+    public function GetData()
     {
         $context = new DAL\InterviewContext();
-        $idList =preg_replace("/[^0-9,]/","",$this->questionString);
-        $PdfData=$context->SelectReportData($idList);
-        print_r($PdfData);
+        $idList = preg_replace("/[^0-9,]/", "", $this->questionString);
+        $this->PdfData = $context->SelectReportData($idList);
+    //    print_r($this->PdfData);
+      //  echo '<Br><Br>';
     }
 
     public function BuildPdf()
@@ -60,13 +60,42 @@ class createPdf
         $pdf->AddPage();
         $pdf->SetFont("Arial", "", 15);
 // breedte, hoogte, tekst, rand, ln, align, fill, link
-        $pdf->Cell(20, 10, 'Mijn eerste pdf', 1,1);
-        $pdf->Cell(0,10,'Lijn2',1,1);
+//        echo 'testbuild<Br><Br>';
+//        print_r($this->PdfData);
+//        echo '<Br><Br>';
+
+        foreach ($this->PdfData as $comp) {
+            foreach ($this->PdfData as $vraag) {
+//                echo $vraag['Vraag'] . '<Br>';
+            }
+        }
+        $pdf->Cell(20, 10, 'Mijn eerste pdf', 1, 1);
+        $pdf->Cell(0, 10, 'Lijn2', 1, 1);
 
         $pdf->Ln(50);
-        $pdf->Cell(0,10,'Lijn3',1,1);
-
-        $pdf->Output('example1.pdf','D');
+        $pdf->Cell(0, 10, 'Lijn3', 1, 1);
+        $this->name = 'example2.pdf';
+        $pdf->Output($this->name,'D');
     }
 
+    public function ExportPDF()
+    {
+        $file = $this->name;
+
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=' . basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            exit;
+        }
+
+    }
 }
