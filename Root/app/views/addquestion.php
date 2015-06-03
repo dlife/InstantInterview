818b95cf89681f1960cf6a677db37afee0107ec7
dlife <dieterbenoot@gmail.com>
2015-06-04 00:56:30 +0200

    Reload page after 3 seconds to view message succes or failure

--- Root/app/views/addquestion.php
+++ Root/app/views/addquestion.php
@@ -24,7 +24,7 @@ if (isset($_POST['selectCompetence'])) {
     $message = <<<DOC
     <br><div class="alert alert-success alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
-        <strong>Success!</strong> Your message has been received. Thanks!<br>
+        <strong>Success!</strong> De nieuwe vraag is toegevoegd aan de databank. De pagina zal binnen enkele seconden vernieuwen<br>
         <stong>Competence: </strong>$competence<br>
         <stong>Question: </strong>$question<br>
     </div>
