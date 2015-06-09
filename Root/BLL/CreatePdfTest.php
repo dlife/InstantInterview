c560404395c9dd7b1fe818d1a0874fd3d337659d
Omirr85 <david.vlaminck@telenet.be>
2015-06-09 10:49:34 +0200

    small changes

--- Root/BLL/CreatePdfTest.php
+++ Root/BLL/CreatePdfTest.php
@@ -11,9 +11,8 @@
 include_once('../vendor/autoload.php');
 
 $pdf = new \BLL\CreatePdf(new \DAL\InterviewContext(), new \BLL\PDF(), "../temp/");
-$array = [1,2,31,4,11,50,15,18,40,7,9];
+$array = [1,2,31,4,11,50,15,18,40,7,9]; // testdata
 $fId = 2;
-
 
 $pdf->parseData($array,$fId);
 $pdf->getData();
@@ -24,6 +23,6 @@ $pdf->OutputDirect();
 // or use this to create the file on the server (does not download automatically)
 //$pdf->OutputToDownloadLater();
 
-// do cleanup
+// do cleanup manually in the test
 $datacontroller = new \BLL\DataController();
 $datacontroller->deleteOldTempFiles();
