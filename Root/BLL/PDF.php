d7eacd52a16027096982c9a09580e75732a8dbdd
Omirr85 <david.vlaminck@telenet.be>
2015-06-09 10:48:52 +0200

    small changes

--- Root/BLL/PDF.php
+++ Root/BLL/PDF.php
@@ -12,19 +12,15 @@ class PDF extends \FPDF
     // Page header
     function Header()
     {
-        // Logo
-        //$this->Image('logo.png',10,6,30);
         // Arial bold 15
         $this->SetFont('Arial', 'B', 15);
-        // Move to the right
-        $this->Cell(70);
         // Title
-        $this->Cell(50, 10, 'Interview Vragen', 0, 0, 'C');
+        $this->Cell(0, 10, 'Interview Vragen', 0, 0, 'C');
         // Line break
         $this->Ln(20);
     }
 
-// Page footer
+    // Page footer
     function Footer()
     {
         // Position at 1.5 cm from bottom
